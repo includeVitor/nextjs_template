0298580c3a0fa71df269a95570223ff555af668e
Vitor Hugo Nunes Da Silva <vitorzcomputacao@gmail.com>
2021-05-18 14:47:25 -0300

    adding product description

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,6 +9,30 @@ const Noop = ({ children }: { children: React.ReactNode }): JSX.Element => (
   <>{children}</>
 )
 
+/**
+ *
+ * Visualização de produto utiliznado <NextSeo>
+ */
+{
+  /* <NextSeo
+  title={product.name}
+  description={product.description}
+  openGraph={{
+    type: 'website',
+    title: product.name,
+    description: product.description,
+    images: [
+      {
+        url: product.images[0]?.url!,
+        width: 800,
+        height: 600,
+        alt: product.name,
+      },
+    ],
+  }}
+/> */
+}
+
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
   const Layout = (Component as any).Layout || Noop
 
